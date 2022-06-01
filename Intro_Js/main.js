339f50214a63bd7268bab7037fe8e14c074719e4
Sebastian Rodriguez <31320972+sebasrbernal@users.noreply.github.com>
2022-05-31 20:24:21 -0500

    Intro Js

---
+++ Intro_Js/main.js
@@ -0,0 +1,21 @@
+//Un lenguaje en escalera//
+
+//Mandar Aletar//
+alert('Hola mundo ')
+alert('Otro alert')
+
+//Mensaje para la consola//
+console.log('este mensaje se vera en la consola')
+console.log('otro mensajen')
+
+//mostrar error en la consola//
+console.error('Error: No se encontro el archivo')
+
+//Mostrar un Warning//
+console.warn("Estas añandiendo muhcas alertas")
+
+//Entrada
+prompt('Escribe tu nombre')
+prompt('Escribe tu apellido')
+
+document.write('Hola mundo')
