017a800aaf89d2b145c374d3b22cf4c0f24177f2
Sebastian Rodriguez <31320972+sebasrbernal@users.noreply.github.com>
2022-06-01 18:37:56 -0500

    Variables

---
+++ 1_Variable_y_Valores/ejercicio.js
@@ -0,0 +1,16 @@
+var mascota1 = 'Pedrito'
+var mascota2 = 'Rufo'
+var mascota3 = 'Chancla'
+
+console.log = ('Mis mascotas son '+ mascota1 + ', ' + mascota2 + 'y ' + mascota3 + '. Les quiero mucho')
+
+mascota1 = prompt('¿Cual es el nombre de tu primer mascota?')
+mascota2 = prompt('¿Cual es el nombre de tu segunda mascota?')
+
+console.log = ('Quiero mucho a mis mascotas '+ mascota1 +'y '+mascota2)
+
+var num = '100'
+console.log = (num)
+console.log = (typeof num)
+
+document.write = ('Mis mascotas son '+ mascota1 + ', ' + mascota2 + 'y ' + mascota3 + '. Les quiero mucho')
---
+++ 1_Variable_y_Valores/main.js
@@ -0,0 +1,59 @@
+// Creacion y asignacion valor a una variable
+var nombre
+nombre = "Sebastian"
+
+var nombreUsuario = "Carlos"
+
+//Strings
+
+var pais='Colombia'
+var fraseSecreta='Primer amigo de la infancia'
+var letra='a'
+var nombrePrincipal=" Dayana"
+
+var info=nombre + pais
+console.log(info)
+//Las variables se pueden reasignar
+
+pais='México'
+letra = 'x'
+
+//Para ver las variables asignadas
+
+console.log(fraseSecreta)
+console.log(letra)
+console.log(nombrePrincipal)
+
+//numero
+
+var edad=20
+var telefono=555555555
+var costo=20
+var resultado= -50
+var promedio=91.8
+
+//reasignacion de variables number
+costo=10
+resultado=0
+
+//Concatenar strings
+var infoNueva=nombre + pais
+console.log(infoNueva)
+console.log( nombreUsuario + '  ' + letra + "Hola mundo")
+console.log(promedio + nombrePrincipal )
+console.log('Mi nombre es '+ nombre + " mi promedio es  " + promedio)
+
+//Pendeintes ->Template strings
+
+
+//Boolean
+var esAdulto=true
+var completado= false
+var graduado= false
+var cumple= true
+var Mexicano=false
+
+// reasignar valores de difentes tipo de dato
+
+esAdulto= 100
+nombre=true
